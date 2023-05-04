2e429c8e60071e6d4d54d3d1cf874fb5c6086c47
Jamal Ahmed <sc19ja@leeds.ac.uk>
2023-05-04 22:09:23 +0100

    moves only in 4 directions now

--- pacman.py
+++ pacman.py
@@ -1,10 +1,7 @@
 import pygame
 import sys
 
-# Initialize Pygame
-pygame.init()
-
-# Constants
+# Game Constants
 WIDTH, HEIGHT = 800, 600
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -13,11 +10,7 @@ BLUE = (0, 0, 255)
 PACMAN_SIZE = 30
 VELOCITY = 5
 
-# Create screen
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Pac-Man")
-
-# Pac-Man character
+# PacMan 
 class PacMan:
     def __init__(self, x, y):
         self.x = x
@@ -27,15 +20,25 @@ class PacMan:
     def draw(self, screen):
         pygame.draw.circle(screen, YELLOW, (self.x, self.y), PACMAN_SIZE // 2)
 
+    def userControl(self,pygame,room):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.move(-1, 0, room)
+        elif keys[pygame.K_RIGHT]:
+            self.move(1, 0, room)
+        elif keys[pygame.K_UP]:
+            self.move(0, -1, room)
+        elif keys[pygame.K_DOWN]:
+            self.move(0, 1, room)
+
     def move(self, dx, dy, room):
         new_x = self.x + dx * self.velocity
         new_y = self.y + dy * self.velocity
-
-        if not self.collides_with_room(new_x, new_y, room):
+        if not self.roomCollision(new_x, new_y, room):
             self.x = new_x
             self.y = new_y
 
-    def collides_with_room(self, x, y, room):
+    def roomCollision(self, x, y, room):
         pacman_rect = pygame.Rect(x - PACMAN_SIZE // 2, y - PACMAN_SIZE // 2, PACMAN_SIZE, PACMAN_SIZE)
         room_rect = pygame.Rect(room.x, room.y, room.width, room.height)
         return not room_rect.contains(pacman_rect)
@@ -52,47 +55,3 @@ class Room:
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
 
-# Create Pac-Man instance
-pacman = PacMan(WIDTH // 2, HEIGHT // 2)
-
-# Create Room instance
-room_width, room_height = 200, 200
-room_x = (WIDTH - room_width) // 2
-room_y = (HEIGHT - room_height) // 2
-room = Room(room_x, room_y, room_width, room_height, BLUE)
-
-# Main game loop
-running = True
-while running:
-    screen.fill(BLACK)
-
-    # Draw Room
-    room.draw(screen)
-
-    # Draw Pac-Man
-    pacman.draw(screen)
-
-    # Event handling
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            sys.exit()
-
-    # Key handling
-    keys = pygame.key.get_pressed()
-    dx, dy = 0, 0
-    if keys[pygame.K_LEFT]:
-        dx = -1
-    if keys[pygame.K_RIGHT]:
-        dx = 1
-    if keys[pygame.K_UP]:
-        dy = -1
-    if keys[pygame.K_DOWN]:
-        dy = 1
-
-    # Move Pac-Man
-    pacman.move(dx, dy, room)
-
-    # Update the display
-    pygame.display.flip()
-    pygame.time.delay(30)
