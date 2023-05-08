fac94a57583e4d9e6bd8678df2eeb92e98510054
Mitali Sen <sc19ms2@leeds.ac.uk>
2023-05-08 14:55:12 +0100

    adding ghosts

---
+++ ghost.py
@@ -0,0 +1,35 @@
+import pygame
+from constants import *
+
+
+# screen dimensions
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+
+# enemy properties
+GHOST_RADIUS = 15
+GHOST_SPEED = 10
+GHOST_START_POS = (SCREEN_WIDTH // 4, SCREEN_HEIGHT // 4)
+GHOST_COLOR = (255, 0, 0)
+
+
+class Ghost:
+    def __init__(self, pos, color):
+        self.pos = pos
+        self.color = color
+        self.radius = GHOST_RADIUS
+        self.speed = GHOST_SPEED
+
+    def move(self, dx, dy):
+        self.pos = (self.pos[0] + dx * self.speed,
+                    self.pos[1] + dy * self.speed)
+
+    def update(self):
+        pass
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, (int(
+            self.pos[0]), int(self.pos[1])), self.radius)
+
+    def get_position(self):
+        return self.pos
